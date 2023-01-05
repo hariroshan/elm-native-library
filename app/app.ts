51051f7961fb0c57b32cdb53a9ab0678de5b4bf1
hariroshan <hariroshanmail@gmail.com>
2023-01-05 13:34:51 +0530

    added typescript

--- app/app.ts
+++ app/app.ts
@@ -1,10 +1,10 @@
 /*
-In NativeScript, the app.js file is the entry point to your application.
+In NativeScript, the app.ts file is the entry point to your application.
 You can use this file to perform app-level initialization, but the primary
 purpose of the file is to pass control to the app’s first module.
 */
 
-import { Application } from '@nativescript/core';
+import { Application } from '@nativescript/core'
 
 Application.run({ moduleName: 'app-root' })
 
--- app/main-page.js
+++
@@ -1,6 +0,0 @@
-import { createViewModel } from './main-view-model';
-
-export function onNavigatingTo(args) {
-  const page = args.object
-  page.bindingContext = createViewModel()
-}
